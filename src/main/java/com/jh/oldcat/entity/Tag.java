67788573e49108dc9adecf0905e898a96769f4cd
oldcat1422 <jh14224@163.com>
2025-08-13 20:48:12 +0800

    添加tag

---
+++ src/main/java/com/jh/oldcat/entity/Tag.java
@@ -0,0 +1,16 @@
+package com.jh.oldcat.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private String tag;
+}
